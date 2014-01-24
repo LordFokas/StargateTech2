ebbb34f1466c361421a83e2c22b40b088f02c58f
LordFokas <lordfokas@gmail.com>
2014-01-24 02:04:37 +0000

    Actually, this is better.

--- SOURCE/stargatetech2/common/reference/ModReference.java
+++ SOURCE/stargatetech2/common/reference/ModReference.java
@@ -4,9 +4,5 @@ public class ModReference {
 	public static final String MOD_ID		= "StargateTech2";
 	public static final String MOD_NAME		= "StargateTech 2";
 	public static final String MOD_VERSION	= "0.6.2-Alpha";
-	
-	// Unnecessary at this point.
-	// And the mod is so much better without BC!
-	//
-	// public static final String DEPENDENCIES = "";
+	public static final String DEPENDENCIES = "after: ThermalExpansion";
 }
