6703b60416a4c6f6d452fd127b0ea06097a73a46
LordFokas <lordfokas@gmail.com>
2014-08-14 02:59:21 +0100

    Updated Forge, got CoFH stuff and TE4 in the mods folder.

--- src/main/java/lordfokas/stargatetech2/core/reference/ModReference.java
+++ src/main/java/lordfokas/stargatetech2/core/reference/ModReference.java
@@ -3,7 +3,6 @@ package lordfokas.stargatetech2.core.reference;
 public class ModReference {
 	public static final String MOD_ID		= "StargateTech2";
 	public static final String MOD_NAME		= "StargateTech 2";
-	public static final String MOD_VERSION	= "0.7.4-Alpha";
-	public static final String DEPENDENCIES = "";
-	// required-after: ThermalExpansion@[3.0.0.2,]
+	public static final String MOD_VERSION	= "0.7.5-Alpha";
+	public static final String DEPENDENCIES = "required-after: ThermalExpansion";
 }
