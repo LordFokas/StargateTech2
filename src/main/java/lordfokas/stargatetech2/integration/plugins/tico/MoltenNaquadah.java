f51a02b32b58fa6ddd064cbdb020000731b3583c
marcin212 <m-d14@o2.pl>
2014-10-30 05:52:46 +0100

    fixed fluid name

--- src/main/java/lordfokas/stargatetech2/integration/plugins/tico/MoltenNaquadah.java
+++ src/main/java/lordfokas/stargatetech2/integration/plugins/tico/MoltenNaquadah.java
@@ -6,14 +6,9 @@ public class MoltenNaquadah extends Fluid {
 	public static final MoltenNaquadah instance = new MoltenNaquadah();
 	
 	public MoltenNaquadah(){
-		super("molten_naquadah");
+		super("moltenNaquadah");
 		setDensity(120000); // Naquadah is heavy as fuck, what did you expect?
 		setLuminosity(15);
 		setTemperature(800); // 800K ~= 525C
-	}
-	
-	@Override
-	public String getUnlocalizedName(){
-		return "StargateTech2:fluid.moltenNaquadah";
 	}
 }
