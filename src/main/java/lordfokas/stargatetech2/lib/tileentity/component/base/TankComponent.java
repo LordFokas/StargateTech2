b637ff0f2f36f0067a180f64dd28921a8245b36c
LordFokas <lordfokas@gmail.com>
2015-02-13 22:13:49 +0000

    Added sync to the TankComponent

--- src/main/java/lordfokas/stargatetech2/lib/tileentity/component/base/TankComponent.java
+++ src/main/java/lordfokas/stargatetech2/lib/tileentity/component/base/TankComponent.java
@@ -1,14 +1,17 @@
 package lordfokas.stargatetech2.lib.tileentity.component.base;
 
+import lordfokas.stargatetech2.lib.tileentity.ISyncedGUI;
 import lordfokas.stargatetech2.lib.tileentity.component.SidedComponent;
 import lordfokas.stargatetech2.lib.tileentity.component.access.ITankComponent;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidTank;
 import net.minecraftforge.fluids.FluidTankInfo;
 
-public class TankComponent extends SidedComponent implements ITankComponent{
+public class TankComponent extends SidedComponent implements ITankComponent, ISyncedGUI.Flow{
+	private static final int[] KEYS = new int[]{0, 1};
 	protected boolean drainable, fillable;
 	protected FluidTank tank;
 	
@@ -56,5 +59,27 @@ public class TankComponent extends SidedComponent implements ITankComponent{
 	@Override
 	public FluidTankInfo getInfo() {
 		return tank.getInfo();
+	}
+
+	@Override
+	public int[] getKeyArray() {
+		return KEYS;
+	}
+
+	@Override
+	public int getValue(int key) {
+		return key == 0 ? tank.getFluidAmount() : tank.getFluid().getFluid().getID();
+	}
+
+	@Override
+	public void setValue(int key, int val) {
+		if(key == 0) set(-1, val);
+		else set(val, -1);
+	}
+	
+	private void set(int f, int a){
+		Fluid fluid = f == -1 ? tank.getFluid().getFluid() : FluidRegistry.getFluid(f);
+		int amount = a == -1 ? tank.getFluidAmount() : a;
+		tank.setFluid(new FluidStack(fluid, amount));
 	}
 }
