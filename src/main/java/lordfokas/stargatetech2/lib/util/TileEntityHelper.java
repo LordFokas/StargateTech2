0cffb94aff3a89fd1fe1499e0c6be60465f98317
LordFokas <lordfokas@gmail.com>
2016-04-23 00:03:58 +0100

    That'd be a certain NPE very soon.

--- src/main/java/lordfokas/stargatetech2/lib/util/TileEntityHelper.java
+++ src/main/java/lordfokas/stargatetech2/lib/util/TileEntityHelper.java
@@ -6,7 +6,7 @@ import net.minecraft.world.World;
 public class TileEntityHelper {
 	public static <TYPE> TYPE getTileEntityAs(World w, int x, int y, int z, Class<TYPE> type){
 		TileEntity te = w.getTileEntity(x, y, z);
-		if(type.isAssignableFrom(te.getClass())){
+		if(te != null && type.isAssignableFrom(te.getClass())){
 			return (TYPE) te;
 		}else return null;
 	}
