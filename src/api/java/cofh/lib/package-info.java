d5f129c15904413c429dce523d580274aec4e4db
LordFokas <lordfokas@gmail.com>
2016-09-02 17:17:09 +0100

    Moved CoFHLib into a compiled JAR

--- src/api/java/cofh/lib/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * (C) 2014-2016 Team CoFH / CoFH / Cult of the Full Hub
- * http://www.teamcofh.com
- */
-@API(apiVersion = CoFHLibProps.VERSION, owner = "CoFHCore", provides = "CoFHLib")
-package cofh.lib;
-
-import net.minecraftforge.fml.common.API;
-
