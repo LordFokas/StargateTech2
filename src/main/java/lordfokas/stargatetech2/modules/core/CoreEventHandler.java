039a787cc7beaff620bac8902e96605d01f70ddc
LordFokas <lordfokas@gmail.com>
2016-09-02 20:06:33 +0100

    Fixes Block Highlight event handler crashing when looking at an Entity

--- src/main/java/lordfokas/stargatetech2/modules/core/CoreEventHandler.java
+++ src/main/java/lordfokas/stargatetech2/modules/core/CoreEventHandler.java
@@ -4,6 +4,7 @@ import lordfokas.stargatetech2.modules.ModuleEnemy;
 import lordfokas.stargatetech2.modules.ModuleTransport;
 import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraftforge.client.event.DrawBlockHighlightEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -16,7 +17,9 @@ public class CoreEventHandler {
 	@SideOnly(Side.CLIENT)
 	public void handleBlockHighlight(DrawBlockHighlightEvent evt){
 		RayTraceResult rtr = evt.getTarget();
-		Block b = Minecraft.getMinecraft().theWorld.getBlockState(rtr.getBlockPos()).getBlock();
+		BlockPos pos = rtr.getBlockPos();
+		if(pos == null) return;
+		Block b = Minecraft.getMinecraft().theWorld.getBlockState(pos).getBlock();
 		if(b == ModuleEnemy.shield || b == ModuleTransport.invisible){
 			evt.setCanceled(true);
 		}
